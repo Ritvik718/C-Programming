e0b0d95a8933bfd767c8b360a1e7567e1663d393
Ritvik718 <113967696+Ritvik718@users.noreply.github.com>
2022-09-25 17:36:58 +0530

    Add files via upload

---
+++ Relational.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main() {
+
+printf("%d \n", 5==5);
+
+
+printf("%d \n", 5<2);
+printf("%d \n", 2<5);
+printf("%d \n", 5<5);
+printf("%d \n", 5<=5);
+
+
+printf("%d \n", 5>2);
+printf("%d \n", 2>5);
+printf("%d \n", 5>5);
+printf("%d \n", 5>=5);
+
+
+printf("%d \n", 5!=5);
+printf("%d \n", 2!=5);
+
+
+return 0;
+}
