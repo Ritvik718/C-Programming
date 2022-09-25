ae0d5b8744a84e24f959ce93700efc63ed73db57
Ritvik718 <113967696+Ritvik718@users.noreply.github.com>
2022-09-25 17:20:45 +0530

    Add files via upload

---
+++ Area Of Circle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+//area of circle
+float main(){
+    float radius ;
+    printf("enter radius");
+    scanf("%f",&radius);
+
+    printf("area is : %f",3.14*radius*radius);
+    return 0;
+
+
+
+}
+
